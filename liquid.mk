8c9426d18248e0e0bbb82b75b0d166f4e1476680
CPA-Poke <brock.shahan@cox.net>
2014-12-28 11:56:19 -0500

    Motorola
    Change-Id: I17aa256a43e409f4599078453b8bb731d4f64613

--- liquid.mk
+++ liquid.mk
@@ -9,9 +9,9 @@ $(call inherit-product, vendor/liquid/config/common_phone.mk)
 $(call inherit-product, vendor/liquid/config/nfc_enhanced.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/moto/shamu/aosp_shamu.mk)
+$(call inherit-product, device/motorola/shamu/aosp_shamu.mk)
 
-DEVICE_PACKAGE_OVERLAYS += device/moto/shamu/overlay-liquid
+DEVICE_PACKAGE_OVERLAYS += device/motorola/shamu/overlay-liquid
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := liquid_shamu
