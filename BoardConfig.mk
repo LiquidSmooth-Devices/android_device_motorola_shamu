cdf0e591c76962b7b4f02fac63a117fcb17e29c6
CPA-Poke <brock.shahan@cox.net>
2015-02-11 09:27:09 -0500

    Bump to 4.9 toolchain
    Change-Id: If8a7c111788672ad3489e3e4c47416897dc75934

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -28,7 +28,7 @@ TARGET_KERNEL_SOURCE := kernel/moto/shamu
 BOARD_KERNEL_IMAGE_NAME := zImage-dtb
 
 # Custom Toolchain Flags
-TARGET_GCC_VERSION_ARM := 4.8-sm
+TARGET_GCC_VERSION_ARM := 4.9-sm
 
 # Liquid Optimizations
 # LIQUIFY := true
