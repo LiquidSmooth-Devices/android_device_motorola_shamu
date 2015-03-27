b40cc3bb10a751d9c8f1015fa70fb081053a6d7a
CPA-Poke <brock.shahan@gmail.com>
2015-03-27 19:36:50 -0400

    Revert to stock toolchain for now
    Change-Id: Iec1d8a260189acacd8ab88170727323166e71798

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -29,7 +29,7 @@ TARGET_KERNEL_SOURCE := kernel/moto/shamu
 BOARD_KERNEL_IMAGE_NAME := zImage-dtb
 
 # Custom Toolchain Flags
-TARGET_GCC_VERSION_ARM := 4.9-sm
+# TARGET_GCC_VERSION_ARM := 4.9-sm
 
 # Liquid Optimizations
 # LIQUIFY := true
