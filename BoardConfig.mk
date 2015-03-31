AndroidOpenDevelopment-Devices/android_device_moto_shamu

shamu: TWRP setup with full crypto support and f2fs
Change-Id: Iba81fc5198ccd7881e99528264d1e344aa797ab4

BoardConfig.mk
@@ -105,6 +105,16 @@ TARGET_RECOVERY_FSTAB = device/moto/shamu/fstab.shamu
 # Ensure f2fstools are built
 TARGET_USERIMAGES_USE_F2FS := true
 
+# TWRP
+TARGET_RECOVERY_DEVICE_DIRS := device/moto/shamu
+DEVICE_RESOLUTION := 1440x2560
+TW_INCLUDE_CRYPTO := true
+TW_INCLUDE_L_CRYPTO := true
+BOARD_HAS_NO_REAL_SDCARD := true
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
+TARGET_RECOVERY_PIXEL_FORMAT := "RGB_565"
+TW_SCREEN_BLANK_ON_BOOT := true
+
 TARGET_RELEASETOOLS_EXTENSIONS := device/moto/shamu
 
 # Support Native Layer RF cutback

LiquidSmooth-Devices/android_device_motorola_shamu

BoardConfig.mk
@@ -134,6 +134,16 @@ endif
 
 TARGET_RELEASETOOLS_EXTENSIONS := device/motorola/shamu
 
+# TWRP
+TARGET_RECOVERY_DEVICE_DIRS := device/motorola/shamu
+DEVICE_RESOLUTION := 1440x2560
+TW_INCLUDE_CRYPTO := true
+TW_INCLUDE_L_CRYPTO := true
+BOARD_HAS_NO_REAL_SDCARD := true
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
+TARGET_RECOVERY_PIXEL_FORMAT := "RGB_565"
+TW_SCREEN_BLANK_ON_BOOT := true
+
 # Support Native Layer RF cutback
 BOARD_USES_CUTBACK_IN_RILD := true
 