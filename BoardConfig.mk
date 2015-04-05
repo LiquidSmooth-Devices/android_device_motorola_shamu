71980168a5c3ba1bf31ac9cc8af2a331cc13fc57
CPA-Poke <brock.shahan@gmail.com>
2015-04-05 02:30:00 -0400

    Use UBER 5.0 toolchain
    Change-Id: Ic8034e8aac227e63923c5379d209849dfe7bbbcd

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -28,7 +28,7 @@ TARGET_KERNEL_SOURCE := kernel/moto/shamu
 BOARD_KERNEL_IMAGE_NAME := zImage-dtb
 
 # Custom Toolchain Flags
-TARGET_GCC_VERSION_ARM := 4.9-sm
+TARGET_GCC_VERSION_ARM := 5.0
 
 # Liquid Optimizations
 # LIQUIFY := true
