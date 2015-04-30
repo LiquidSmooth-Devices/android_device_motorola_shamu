075cc13ee36e3c06b54d088839e59f2915423369
CPA-Poke <brock.shahan@gmail.com>
2015-04-30 02:25:03 -0400

    Use prebuilt cache
    Change-Id: I2da78139ad83d683662c4a5bf233571203ce53b2

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -22,6 +22,8 @@ TARGET_CPU_VARIANT := krait
 
 TARGET_NO_BOOTLOADER := true
 
+USE_PREBUILT_CACHE=1
+
 # Inline kernel building
 TARGET_KERNEL_CONFIG := shamu_defconfig
 TARGET_KERNEL_SOURCE := kernel/moto/shamu
